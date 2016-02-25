14b2e88bb3e4587187fc9c3a3418bb6d6d3e434c
nboehning <nboehning@gmail.com>
2016-02-25 03:38:30 -0600

    Completed the videos
    Completed the videos

---
+++ GestureHomework/Assets/_Scripts/1FingerGesture/OneFingerGesture.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneFingerGesture : MonoBehaviour
+{
+    private GameObject objectToMove;
+    private Vector2 prevPosition;
+    private Vector2 curPosition;
+    private float touchDelta;
+    public int iComfort;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.touchCount == 1)
+	    {
+	        if (Input.GetTouch(0).phase == TouchPhase.Began)
+	        {
+	            prevPosition = Input.GetTouch(0).position;
+	        }
+	        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+	        {
+	            curPosition = Input.GetTouch(0).position;
+
+	            touchDelta = curPosition.magnitude - prevPosition.magnitude;
+
+	            if (Mathf.Abs(touchDelta) > iComfort)
+	            {
+	                Debug.Log("Swipe");
+
+	                if (touchDelta > 0)
+	                {
+                        if (Mathf.Abs(curPosition.x - prevPosition.x) > Mathf.Abs(curPosition.y - prevPosition.y))
+                        {
+                            Debug.Log("Right");
+                        }
+                        else
+                        {
+                            Debug.Log("Top");
+                        }
+                    }
+	                else
+	                {
+	                    if (Mathf.Abs(curPosition.x - prevPosition.x) > Mathf.Abs(curPosition.y - prevPosition.y))
+	                    {
+	                        Debug.Log("Right");
+	                    }
+	                    else
+	                    {
+	                        Debug.Log("Top");
+	                    }
+	                }
+	            }
+	            else
+	            {
+	                if (Input.GetTouch(0).tapCount == 1)
+	                {
+	                    Debug.Log("Detected 1 finger single tap");
+
+	                    RaycastHit hit;
+	                    Ray ray;
+
+	                    ray = Camera.current.ScreenPointToRay(Input.GetTouch(0).position);
+	                    if (Physics.Raycast(ray, out hit))
+	                    {
+	                        // Select an object
+	                        Debug.Log(hit.transform.name);
+
+	                        if (objectToMove != null && hit.transform.name != objectToMove.transform.name)
+	                        {
+	                            // Stop the particle system
+	                            objectToMove.GetComponent<ParticleSystem>().Play();
+	                        }
+
+	                        objectToMove = GameObject.Find(hit.transform.name);
+	                        objectToMove.GetComponent<ParticleSystem>().Play();
+	                    }
+	                }
+	                else if (Input.GetTouch(0).tapCount == 2)
+	                {
+	                    if (objectToMove != null)
+	                    {
+	                        Vector3 pos;
+	                        pos.x = Input.GetTouch(0).position.x;
+	                        pos.y = Input.GetTouch(0).position.y;
+
+	                        pos.z = Mathf.Abs(Camera.current.transform.position.z - objectToMove.transform.position.z);
+
+	                        objectToMove.transform.position = Camera.current.ScreenToWorldPoint(pos);
+	                    }
+	                }
+	            }
+	        }
+	    }
+	}
+}
---
+++ GestureHomework/Assets/_Scripts/2FingerGesture/TwoFingerGesture.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoFingerGesture : MonoBehaviour
+{
+
+	public GameObject objectToRotate;
+	private Vector2 curPosition;
+	private Vector2 prevPosition;
+	private float touchDelta;
+	public float comfortZone = 2;
+	public Camera myCamera;
+    public int zoom = 5;
+    private float angle;
+    private int rotateSpeed = 8;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved &&
+			Input.GetTouch(1).phase == TouchPhase.Moved)
+		{
+			curPosition = Input.GetTouch(0).position - Input.GetTouch(1).position;
+			prevPosition = (Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) -
+						   (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition);
+
+			touchDelta = curPosition.magnitude - prevPosition.magnitude;
+
+		    angle = Vector2.Angle(prevPosition, curPosition);
+
+		    if (angle > 0.1f)
+		    {
+		        // Rotation
+		        Debug.Log(Vector3.Cross(curPosition, prevPosition));
+
+		        if (Vector3.Cross(curPosition, prevPosition).z < 0)
+		        {
+		            Debug.Log("Counter-Clockwise");
+                    objectToRotate.transform.Rotate(Vector3.up, angle * -1 * rotateSpeed);
+                }
+		        else
+		        {
+		            Debug.Log("Clockwise");
+		            objectToRotate.transform.Rotate(Vector3.up, angle * rotateSpeed);
+		        }
+		    }
+
+			if (Mathf.Abs(touchDelta) > comfortZone)
+			{
+				
+				if (touchDelta > 0)
+				{
+					// Zoom in
+					Debug.Log("Zoom In");
+
+				    myCamera.fieldOfView = Mathf.Clamp(Mathf.Lerp(myCamera.fieldOfView, myCamera.fieldOfView - Mathf.Abs(touchDelta) * zoom,
+				        Time.deltaTime * zoom), 10, 17);
+				}
+				else
+				{
+					// Zoom out
+					Debug.Log("Zoom Out");
+
+				    myCamera.fieldOfView = Mathf.Clamp(Mathf.Lerp(myCamera.fieldOfView, myCamera.fieldOfView + Mathf.Abs(touchDelta)*zoom,
+				        Time.deltaTime*zoom), 10, 17);
+				}
+			}
+
+		}
+	}
+}
